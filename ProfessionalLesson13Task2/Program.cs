f50e15208cf0e0c5245b4f8c570f957e085560d1
Nikita Lebedenko <nlebn001@gmail.com>
2022-01-08 21:55:40 +0100

    Was adjusted task 2.

--- ProfessionalLesson13Task2/Program.cs
+++ ProfessionalLesson13Task2/Program.cs
@@ -15,19 +15,34 @@ namespace ProfessionalLesson13Task2
         static void Main(string[] args)
         {
             func = StringHandler;
-            func.BeginInvoke("Line", Callback, func);
-            Console.ReadKey();
+            while (true)
+            {
+                Console.WriteLine("\nInput smth.");
+                string str = Console.ReadLine();
+                func.BeginInvoke(str, Callback, func);
+                Waiting();
+            }
         }
 
         static string StringHandler(string str)
         {
-            return $"Was inputed '{str}' string.";
+            Thread.Sleep(3000);
+            return $"\nWas inputed '{str}' string.";
         }
 
         static void Callback(IAsyncResult asyncResult)
         {
             string s = (asyncResult.AsyncState as Func<string, string>).EndInvoke(asyncResult);
             Console.WriteLine(s);
+        }
+
+        static void Waiting()
+        {
+            for (int i = 0; i < 28; i++)
+            {
+                Thread.Sleep(200);
+                Console.Write(". ");
+            }
         }
 
     }
